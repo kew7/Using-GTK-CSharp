411d00c81f0b52947f5484ce7f803b0400c86781
kew7 <kew7@yandex.ru>
2021-01-20 05:55:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Gtk;
+
+namespace USBRead
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            USB win = new USB();
+            win.Show();
+           // win.Resize(320, 200);
+            win.ShowAll();
+            Application.Run();
+        }
+    }
+}
---
+++ USB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Gtk;
+//using Mono.Unix.Native;
+
+public partial class USB : Gtk.Window
+{
+   
+    public USB() : base(WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void On_bStop(object sender, EventArgs e)
+    {
+        label.Text = "Останов...";
+        ShowAll();
+    }
+
+    protected void On_bPusk(object sender, EventArgs e)
+    {
+        label.Text = "Запуск...";
+        ShowAll();
+    }
+}
